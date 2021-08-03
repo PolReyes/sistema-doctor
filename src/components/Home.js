fd89930727ab577959cb3b0d15ee595fd8c219ba
PolReyes <wpol.reyes@gmail.com>
2021-08-03 15:37:51 -0500

    Modificaciones al component home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-import { Button, Card, CardContent, Typography } from '@material-ui/core';
+import { Button, Card, CardContent, Grid, TextField, Typography, Box } from '@material-ui/core';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
@@ -17,12 +17,18 @@ import Select from '@material-ui/core/Select';
 import { Link } from 'react-router-dom';
 
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
   table: {
     minWidth: 650,
   },
   card: {
     backgroundColor:'#0033A0',
+    margin: '20px'
+  },
+  card2: {
     margin: '20px'
   },
   cardInfo: {
@@ -33,12 +39,33 @@ const useStyles = makeStyles({
     textAlign:'left',
     color:'#fff'
   },
+  title2:{
+    textAlign:'left',
+    color:'#0033A0',
+  },
   btn:{
     backgroundColor:'#00E1CD',
     color:'#fff',
     float:'right'
+  },
+  formControl: {
+    float:'left',
+    minWidth: 320,
+    margin: '20px',
+    //flexGrow:1
+  },
+  selectEmpty: {
+  },
+  label: {
+    float:'left',
+    marginLeft: '20px',
+  },
+  textField: {
+    float:'left',
+    minWidth: 320,
+    marginLeft: '20px',
   }
-});
+}));
 
 function createData(name, calories, fat, carbs, protein) {
   return { name, calories, fat, carbs, protein };
@@ -54,10 +81,15 @@ const rows = [
 const Home = () => {
     const classes = useStyles();
     const name = localStorage.getItem('user');
-  const [age, setAge] = React.useState('');
+    
+    const [tipoPaciente, setTipoPaciente] = React.useState('');
+    const [tipoConsulta, settipoConsulta] = React.useState('');
 
-  const handleChange = (event) => {
-    setAge(event.target.value);
+  const handleChangePaciente = (event) => {
+    setTipoPaciente(event.target.value);
+  };
+  const handleChangeConsulta = (event) => {
+    settipoConsulta(event.target.value);
   };
     return (
         <>
@@ -74,8 +106,127 @@ const Home = () => {
             <Card className={classes.cardInfo}>
             <CardContent >
             <Typography variant="h6">
-                02 de Julio del 2021
+                02 de Julio del 2021 Resumen del mes
             </Typography>
+            <Typography variant="h6">
+                total de pacientes: 50
+            </Typography>
+          
+            </CardContent>
+            </Card>
+            
+        
+        </Card>
+        
+        <Grid container>
+              <Grid item md={4} xs={12}>
+              <Box m={2} p={2} boxShadow={1} >
+              <Typography variant="h6" className={classes.title2}>
+              Buscar por filtros
+            </Typography>
+            <form>
+            <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+        <FormControl className={classes.formControl}>
+                  <InputLabel id="label-tipo-paciente">Tipo de paciente</InputLabel>
+                  <Select
+                  labelId="label-tipo-paciente"
+                  id="tipo-pacinte"
+                  value={tipoPaciente}
+                  onChange={handleChangePaciente}
+                  >
+                  <MenuItem value={'Seguro'}>Seguro</MenuItem>
+                  <MenuItem value={'Particular'}>Particular</MenuItem>
+                  </Select>
+                  </FormControl>
+        </Grid>
+      </Grid>
+    </div>
+    <div className={classes.root}>
+                  <FormControl className={classes.formControl}>
+                  <InputLabel id="label-tipo-consulta">Tipo de consulta</InputLabel>
+                  <Select
+                  labelId="label-tipo-consulta"
+                  id="tipo-consulta"
+                  value={tipoConsulta}
+                  onChange={handleChangeConsulta}
+                  >
+                  <MenuItem value={'Cita'}>Cita</MenuItem>
+                  <MenuItem value={'Procedimiento'}>Cirujía/Otros</MenuItem>
+                  <MenuItem value={'Bonos'}>Bonos</MenuItem>
+                  </Select>
+                  </FormControl>
+              </div>
+              <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+        <InputLabel className={classes.label}>Fecha</InputLabel>
+        </Grid>
+        <Grid item xs={12}>
+        
+          <TextField   className={classes.textField} type="date" name=""  />
+        </Grid>
+      </Grid>
+    </div>
+    <div className={classes.root}>
+                  <FormControl className={classes.formControl}>
+                  <InputLabel id="label-tipo-consulta">Seleccionar Mes</InputLabel>
+                  <Select
+                  labelId="label-tipo-consulta"
+                  id="tipo-consulta"
+                  value={tipoConsulta}
+                  onChange={handleChangeConsulta}
+                  >
+                  <MenuItem value={'Cita'}>Cita</MenuItem>
+                  <MenuItem value={'Procedimiento'}>Cirujía/Otros</MenuItem>
+                  <MenuItem value={'Bonos'}>Bonos</MenuItem>
+                  </Select>
+                  </FormControl>
+              </div>
+     <Button variant="contained" >Buscar</Button>
+            </form>
+              </Box>
+              </Grid>
+              <Grid item md={8} xs={12}>
+                <Box m={2} p={3} boxShadow={1}>
+                  <h3>Vista general</h3>
+                  <hr></hr>
+                  <TableContainer >
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Dessert (100g serving)</TableCell>
+            <TableCell align="right">Calories</TableCell>
+            <TableCell align="right">Fat&nbsp;(g)</TableCell>
+            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.name}>
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+              <TableCell align="right">{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+                </Box>
+              </Grid>
+        </Grid> 
+        
+            
+            <Card className={classes.card2}>
+            <CardContent >
+           
+           
             <TableContainer >
       <Table className={classes.table} aria-label="simple table">
         <TableHead>
@@ -102,45 +253,6 @@ const Home = () => {
         </TableBody>
       </Table>
     </TableContainer>
-            </CardContent>
-            </Card>
-            
-        
-        </Card>
-            
-        
-            
-            <Card>
-            <CardContent >
-            <Typography variant="h6">
-               Buscar por mes
-            </Typography>
-            <FormControl>
-        <InputLabel id="demo-simple-select-label">Age</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={age}
-          onChange={handleChange}
-        >
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
-        </Select>
-      </FormControl>
-      <FormControl>
-        <InputLabel id="demo-simple-select-label">Age</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={age}
-          onChange={handleChange}
-        >
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
-        </Select>
-      </FormControl>
            
             </CardContent>
             </Card>
